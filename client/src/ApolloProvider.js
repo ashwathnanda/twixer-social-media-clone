2739ae6fe8477bea03c57e7ff3f34b39639311be
Ashwath Nanda <ashwath5897@gmail.com>
2021-11-01 17:48:19 +0530

    Change server to heroku url

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -9,7 +9,7 @@ import {
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://twixer-smc.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
